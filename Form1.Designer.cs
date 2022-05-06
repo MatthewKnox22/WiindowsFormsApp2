bd52769c680e4a7c8fedfafec5d6c761bc8f93be
Matthew S Knox <91904135+MatthewKnox22@users.noreply.github.com>
2022-05-06 11:00:08 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class FormPigLatin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(37, 29);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(247, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(37, 123);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(247, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(89, 73);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(131, 23);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "Translate to Pig Latin";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // FormPigLatin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 184);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "FormPigLatin";
+            this.Text = "Pig Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnTranslate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class FormPigLatin : Form
+    {
+        const string EXTENSION = "ay";
+        public FormPigLatin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            string translation = "";
+            string input = txtInput.Text;
+            string[] tempInput = input.Split(' ');
+
+            for (int i = 0; i < tempInput.Length; i++)
+            {
+                translation += getPigLatin(tempInput[i]);
+            }
+            txtOutput.Text = translation;
+        }
+        private string getPigLatin(string input)
+        {
+            string word;
+            string firstLetter = input.Substring(0, 1);
+
+            word = input.Substring(1) + firstLetter + EXTENSION + " ";
+
+            return word;
+        }
+    }
+}
